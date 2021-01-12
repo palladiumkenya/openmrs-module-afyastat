429840b32d96d1f98d41895d10911edfadf06de5
makombe <kennedymakombe@gmail.com>
2021-01-12 10:41:10 +0300

    Initial commit

---
+++ api/src/main/java/org/openmrs/module/afyastat/api/impl/AfyastatServiceImpl.java
@@ -0,0 +1,37 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.afyastat.api.impl;
+
+import org.openmrs.api.UserService;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.afyastat.api.AfyastatService;
+import org.openmrs.module.afyastat.api.dao.AfyastatDao;
+
+public class AfyastatServiceImpl extends BaseOpenmrsService implements AfyastatService {
+	
+	AfyastatDao dao;
+	
+	UserService userService;
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setDao(AfyastatDao dao) {
+		this.dao = dao;
+	}
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+}
