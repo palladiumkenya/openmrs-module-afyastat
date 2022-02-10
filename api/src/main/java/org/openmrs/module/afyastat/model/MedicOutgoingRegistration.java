f4904d3f697a1e781de2b2115d32af086e94b3cc
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-02-10 10:53:18 +0300

    camelcase fix, table name fix, removed annotations, added hbm file, included hbm file in config.xml

--- api/src/main/java/org/openmrs/module/afyastat/model/MedicOutgoingRegistration.java
+++ api/src/main/java/org/openmrs/module/afyastat/model/MedicOutgoingRegistration.java
@@ -1,39 +1,28 @@
 package org.openmrs.module.afyastat.model;
 
 import org.openmrs.BaseOpenmrsData;
-import org.openmrs.BaseOpenmrsMetadata;
-import org.openmrs.BaseOpenmrsObject;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity(name = "afyastat.MedicOutgoingRegistration")
-@Table(name = "afyastat_medicoutgoingregistration")
 public class MedicOutgoingRegistration extends BaseOpenmrsData implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue
-	@Column(name = "id")
 	private Integer id;
 	
-	@Column(name = "patient_id", nullable = false)
-	private Integer patient_id;
+	private Integer patientId;
 	
-	@Column(name = "cht_ref", nullable = true, length = 512)
-	private String cht_ref;
+	private String chtRef;
 	
-	@Column(name = "kemr_ref", nullable = false, length = 512)
-	private String kemr_ref;
+	private String kemrRef;
 	
-	@Column(name = "purpose", nullable = false, length = 512)
 	private String purpose;
 	
-	@Column(name = "payload", nullable = false, length = 512)
 	private String payload;
 	
-	@Column(name = "status", nullable = false)
 	private Integer status;
 	
 	/**
@@ -52,28 +41,28 @@ public class MedicOutgoingRegistration extends BaseOpenmrsData implements Serial
 		this.id = id;
 	}
 	
-	public Integer getPatient_id() {
-		return patient_id;
+	public Integer getPatientId() {
+		return patientId;
 	}
 	
-	public void setPatient_id(Integer patient_id) {
-		this.patient_id = patient_id;
+	public void setPatientId(Integer patient_id) {
+		this.patientId = patientId;
 	}
 	
-	public String getCht_ref() {
-		return cht_ref;
+	public String getChtRef() {
+		return chtRef;
 	}
 	
-	public void setCht_ref(String cht_ref) {
-		this.cht_ref = cht_ref;
+	public void setChtRef(String chtRef) {
+		this.chtRef = chtRef;
 	}
 	
-	public String getKemr_ref() {
-		return kemr_ref;
+	public String getKemrRef() {
+		return kemrRef;
 	}
 	
-	public void setKemr_ref(String kemr_ref) {
-		this.kemr_ref = kemr_ref;
+	public void setKemrRef(String kemrRef) {
+		this.kemrRef = kemrRef;
 	}
 	
 	public String getPurpose() {
