b940f12061473a605487ba0d914ae98e3446b5c6
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-03-18 16:59:21 +0300

    1. Fixed a problem with the client name starting with null on queue display  e.g nullMary Wambui. 2. Fixed method description for method 'queueClientForOutgoingRegistration'

--- api/src/main/java/org/openmrs/module/afyastat/util/MedicDataExchange.java
+++ api/src/main/java/org/openmrs/module/afyastat/util/MedicDataExchange.java
@@ -2360,7 +2360,7 @@ public class MedicDataExchange {
 	/**
 	 * Manually queue a client into the outgoing queue
 	 * 
-	 * @param clientId the client ID
+	 * @param clientId the patient ID
 	 * @param purpose the purpose
 	 * @return true on success or false on failure
 	 */
